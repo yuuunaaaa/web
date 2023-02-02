f7bf7ec5e179bf2c1442a55ea7de6b2700532901
yuuunaaaa <124111014+yuuunaaaa@users.noreply.github.com>
2023-02-02 17:36:43 +0900

    day04 프로젝트

---
+++ day04/out.js
@@ -0,0 +1,18 @@
+    // 함수는 여러절차작 있는 처리를 이름을 부여하여 기능을 정의!
+        // function
+        function 빨래하다() {
+            console.log("1. 먼지를 털자.")
+            console.log("2. 물에 집어 넣는다.")
+            console.log("3. 비누를 바른다.")
+            console.log("4. 오물조물 비빈다.")
+            console.log("5. 물에 여러번 헹군다.")
+            console.log("6. 짠다.")
+            console.log("7. 넌다.")
+         }
+    
+          function 청소하다() {
+            console.log("1. 돌돌이를 꺼낸다.")
+            console.log("2. 돌돌이로 바닥의 먼지를 돌돌한다.")
+            console.log("3. 바닥을 청소용 물티슈로 닦는다.")
+            console.log("4. 책상을 정리한다.")
+         }
---
+++ day04/out2.js
@@ -0,0 +1,13 @@
+function 여행하다() {
+    console.log("1. 여행지를 정한다.")
+    console.log("2. 일정을 짠다.")
+    console.log("3. 필요한 것을 산다.")
+    console.log("4. 짐을 싼다.")
+    console.log("5. 여행을 한다.")
+}
+
+function 쉬다() {
+    console.log("1. 침대로 간다.")
+    console.log("2. 눕는다.")
+    console.log("3. 폰을 한다.")
+}
